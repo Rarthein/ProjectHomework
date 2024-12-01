537536dcb7136d2d61200f7f5f1b9bb5f373b8a7
Vladislav <slim-play@mail.ru>
2024-12-01 21:56:28 +0300

    add func get_date and other fixes

--- src/widget.py
+++ src/widget.py
@@ -1,22 +1,32 @@
 def mask_account_card(account_data: str) -> str:
-    if "Visa Platinum" in account_data:
-        """Функция маски номеров карты или счета"""
-        # Маскируем номер карты для Visa Platinum
-        parts = account_data.split()
-        card_number = parts[-1]
-        masked_number = card_number[:4] + " " + card_number[4:6] + "** " + "**** " + card_number[-4:]
-        return masked_number
-    elif "Maestro" in account_data:
-        # Маскируем номер карты для Maestro
-        parts = account_data.split()
-        card_number = parts[-1]
-        masked_number = card_number[:4] + " " + card_number[4:6] + "** " + "**** " + card_number[-4:]
-        return masked_number
-    elif "Счет" in account_data:
-        # Маскируем номер счета
-        parts = account_data.split()
+    parts = account_data.split()
+
+    if len(parts) < 2:
+        return "Неверный ввод"
+
+    card_name = " ".join(parts[:-1])
+
+    if card_name.lower() == "счет":
         account_number = parts[-1]
         masked_account = "**" + account_number[-4:]
-        return masked_account
+        return f"{card_name.lower()} {masked_account}"
     else:
-        return "Ошибка ввода"
+        card_number = parts[-1]
+        masked_number = card_number[:4] + " " + card_number[4:6] + "** " + "**** " + card_number[-4:]
+        return f"{card_name.lower()} {masked_number}"
+
+
+print(mask_account_card('Visa Platinum 8990922113665229'))
+
+def get_date(input_date: str) -> str:
+
+    string_date = input_date.split('T')
+    date_part = string_date[0]
+    final_date = date_part.split('-')
+    day = final_date[2]
+    month = final_date[1]
+    year = final_date[0]
+    return f"{day}.{month}.{year}"
+
+
+print(get_date("2024-03-11T02:26:18.671407"))
