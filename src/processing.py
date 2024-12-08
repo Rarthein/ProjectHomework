26c5cfc32b6fa9a3251af8450771b0f5eb61aa69
Vladislav <slim-play@mail.ru>
2024-12-08 21:39:47 +0300

    begin homework 10_1

--- src/processing.py
+++ src/processing.py
@@ -14,4 +14,4 @@ print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03
                        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
                        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}], "CANCELED"))
 
-asd
+1
