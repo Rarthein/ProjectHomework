e0fa920f81d08edd069e59f16ef67c06909c476d
Vladislav <slim-play@mail.ru>
2024-12-23 16:35:54 +0300

    Задачи с уроков

--- tasks/lesson_task1.py
+++ tasks/lesson_task1.py
@@ -1,20 +1,37 @@
-def is_even(x):
-    return x % 2 == 0
+# def is_even(x):
+#     return x % 2 == 0
+#
+# result_filter = list(filter(is_even, range(20)))
+#
+# print(result_filter)
+#
+# def double(x):
+#     return [x, x]
+#
+# print(double(11))
+#
+# result_map = list(map(double, result_filter))
+# print(result_map)
+#
+# from itertools import chain
+#
+# result_chain = list(chain(*result_map))
+#
+# print(result_chain)
 
-result_filter = list(filter(is_even, range(20)))
+'''1'''
 
-print(result_filter)
+result_cubed = print(list(x ** 3 for x in range(11) if x % 2 == 0))
 
-def double(x):
-    return [x, x]
+'''2'''
 
-print(double(11))
+num_list = [2, 3, -5, 8, -4, 1, 0, -10, 12]
 
-result_map = list(map(double, result_filter))
-print(result_map)
 
-from itertools import chain
+def sum_positive(num_list):
+    return sum(x ** 2 for x in num_list if x > 0)
 
-result_chain = list(chain(*result_map))
+print(sum_positive(num_list))
 
-print(result_chain)
+'''3'''
+
