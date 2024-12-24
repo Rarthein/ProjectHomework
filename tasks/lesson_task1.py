0947fec4dc0f710a0df112edcbe3342a92effe1e
Vladislav <slim-play@mail.ru>
2024-12-24 07:33:11 +0300

    задачи с уроков

--- tasks/lesson_task1.py
+++ tasks/lesson_task1.py
@@ -35,3 +35,35 @@ print(sum_positive(num_list))
 
 '''3'''
 
+result_vowels = print(list(x for x in "hello" if x in ['a', 'e', 'i', 'o', 'u']))
+
+'''4'''
+
+num = range(50)
+result_arithmetic = list(filter(lambda x: x % 3 ==0 or x% 5 == 0, num))
+av_arithmetic = sum(result_arithmetic) / len(result_arithmetic)
+print(round(av_arithmetic))
+
+'''5'''
+
+from itertools import chain
+
+list1 = [1, 2, 3, 4]
+list2 = [3, 4, 5, 6]
+list3 = [5, 6, 7, 8]
+
+join_list = list(set(chain(list1, list2, list3)))
+print(join_list)
+
+'''6'''
+
+people = [
+    {"name": "Alice", "age": 25},
+    {"name": "Bob", "age": 30},
+    {"name": "Charlie", "age": 35},
+    {"name": "David", "age": 30},
+    {"name": "Eve", "age": 25},
+]
+
+filtered_people = list(filter(lambda x: x["age"] == 30, people))
+print(filtered_people)
