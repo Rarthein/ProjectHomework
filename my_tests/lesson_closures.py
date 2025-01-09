f2be6a7a79b430b3da9051b56a0fdcb81f34eb96
Vladislav <slim-play@mail.ru>
2025-01-09 16:35:57 +0300

    some tests

---
+++ my_tests/lesson_closures.py
@@ -0,0 +1,14 @@
+def names():
+    all_names = []
+
+    def inner(name):
+        all_names.append(name)
+        return all_names
+
+    return inner
+
+if __name__ == '__main__':
+    students = names()
+    print(students('Vasya'))
+    print(students('Petya'))
+    print(students('Dima'))
