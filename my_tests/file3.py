df60224c091123c71384521a70f26d045f2812a7
Vladislav <slim-play@mail.ru>
2025-01-09 13:30:26 +0300

    some tests

--- my_tests/file3.py
+++ my_tests/file3.py
@@ -1,4 +1,17 @@
+# def create_counter():
+#     count = [0]
+#
+#     def counter():
+#         count[0] += 1
+#         return count[0]
+#
+#     return counter
+#
+# my_counter = create_counter()
+#
+# print(my_counter())
+# print(my_counter())
+# print(my_counter())
+# print(my_counter())
+# print(my_counter())
 
-print(100500)
-
-print(__name__)
